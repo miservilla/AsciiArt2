6a73f71fdcd8c9364d99aa4e6a679c18017de2a2
miservilla <miservilla@gmail.com>
2016-08-23 21:24:20 -0600

    Initial commit

---
+++ src/AsciiArt.java
@@ -0,0 +1,24 @@
+/**
+ * @version 2016-08-23
+ * @author Michael Servilla
+ */
+public class AsciiArt {
+
+    /**
+     * Prints my name in ASCII Art to the console.
+     * @param args Comand-Line arguments are ignored.
+     */
+
+    public static void main(String[] args){
+        System.out.println("____   ____     ____    ____          ________");
+        System.out.println("I  \\  /   I     I  I    I  I  ____    I   ____|");
+        System.out.println("I   \\/    I     I__I    I  I /   /    I  I");
+        System.out.println("I         I     ____    I   /   /     I  I ");
+        System.out.println("I  I\\/I   I     I  I    I      /      I  I__");
+        System.out.println("I  I  I   I     I  I    I     /       I   __|");
+        System.out.println("I  I  I   I     I  I    I      \\      I  I");
+        System.out.println("I  I  I   I     I  I    I  I\\   \\     I  I____");
+        System.out.println("I__I  I___I     I__I    I__I \\___\\    I_______|");
+
+    }
+}
